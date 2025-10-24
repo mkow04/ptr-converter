98c2fcb4f027803d3f805a9ebef8ff8e140d9a24
mkow <mkow@mkowalski.xyz>
2025-10-24 03:02:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# PTR Converter v0.1
+Python script for converting A and AAAA dns records into PTR records, for use in Bind9 DNS.
+
+
+> [!CAUTION]
+> The script is in early development. Don't expect it to work well, or work at all.
+
+## Dependencies:
+- python >= 3.13
+- [python-colorclasses](https://github.com/mkow04/python-colorclasses) >= v1.1 <- expected to be in the same folder as the script
+
---
+++ ptr-converter.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+__package_name__ = "ptr-converter"
+__version__ = "v0.1"
+__license__ = "Unlicense"
+
+__author__ = "mkow04"
+__email__ = "maciejkowalski04@proton.me"
+
+import sys
+from colorclasses import Color, Effect
+
+
+def zone_deduper(zone: list) -> list:
+    """
+    Function deduplicating a zone, by asking the user which record to keep,
+    and turning it into a list of [ip: hostname] entries
+    """
+
+    a_ips = [i[3] for i in zone]
+
+    a_duped = {}
+    a_deduped = []
+
+    for i in zone:
+        if a_ips.count(i[3]) > 1:
+            a_duped.setdefault(i[3], [])
+            a_duped[i[3]].append(i)
+        else:
+            a_deduped.append(i)
+
+    for i in a_duped:
+        print(f"\n{Color.BLUE}==> {Color.WHITE}Duplicated records found for IP {Color.GRAY}'{i}'{Color.WHITE}: {Effect.RESET}\n")
+        for j in a_duped[i]:
+            print(f"{a_duped[i].index(j)}: {j[0]}")
+        input_index = int(input(f"\n{Color.GREEN}==> {Color.WHITE}Choose which one to keep: {Effect.RESET}"))
+        a_deduped.append(a_duped[i][input_index])
+
+    return [[i[3], i[0]] for i in a_deduped]
+
+
+def make_ipv4_ptr(ipv4_list: list, domain: str) -> list:
+    """
+    Function making a PTR like dictionary of tuples with entries like this: ("ptr zone", ("3rd octet", "fqdn"))
+    """
+    result = {}
+
+    for ip, hostname in ipv4_list:
+        octets = ip.split('.')
+        zone = ".".join([octets[2], octets[1], octets[0]]) + ".in-addr.arpa"
+
+        key = octets[3]
+
+        fqdn = hostname + "." + domain + "."
+
+        result.setdefault(zone, {})[key] = fqdn
+
+    return result
+
+
+def main():
+    """ Main """
+
+    # Get zone from the user
+
+    print(f"\n{Color.GREEN}==> {Color.WHITE}Provide A and AAAA records to be converted, press {Color.GRAY}'CTRL+D'{Color.WHITE} on a new line to submit: {Effect.RESET}\n")
+
+    zones = sys.stdin.read()
+
+    zone_list = zones.splitlines()
+
+    # Filter zone
+
+    zone_list = list(set(zone_list))  # Remove duplicates
+
+    zone_list = [i for i in zone_list if i != ""]  # Remove empty lines
+
+    zone_list = [i for i in zone_list if i[0] != ";"]  # Remove comments
+
+    zone_list = [i.split() for i in zone_list]  # Split each line into a list
+
+    # Separate A and AAAA records
+
+    a_list = []
+    aaaa_list = []
+
+    for i in zone_list:
+        if i[2] == "A":
+            a_list.append(i)
+        elif i[2] == "AAAA":
+            aaaa_list.append(i)
+
+    # Deduplicate
+
+    a_list = zone_deduper(a_list)
+    aaaa_list = zone_deduper(aaaa_list)
+
+    # Get domain name
+
+    domain = input(f"\n\n{Color.GREEN}==> {Color.WHITE}Provide a domain name for all the hosts in the provided zone: {Effect.RESET}\n")
+
+    # Make PTR records
+
+    a_ptr_list = make_ipv4_ptr(a_list, domain)
+
+    # Give the result
+
+    # IPv4
+
+    print(f"\n\n{Color.YELLOW}==== {Color.WHITE}IPv4 Results:{Color.YELLOW} ===={Effect.RESET}\n")
+
+    for k in a_ptr_list:
+        v = a_ptr_list[k]
+        print(f"\n{Effect.BOLD}{k}{Effect.RESET}")
+        for vk in v:
+            vv = v[vk]
+            print(f"{vk}\tIN\tPTR\t{vv}")
+
+    # Files for bind
+
+    print(f"\n\n{Color.YELLOW}==== {Color.WHITE}Bind config:{Color.YELLOW} ===={Effect.RESET}\n")
+
+    for k in a_ptr_list:
+        print(f"zone {k} IN {"{"}")
+        print("	type master;")
+        print(f"	file \"/etc/bind/master/{k}\";")
+        print("};")
+
+    print(f"\n\n{Color.YELLOW}==== {Color.WHITE}DONE{Color.YELLOW} ===={Effect.RESET}\n")
+
+
+if __name__ == "__main__":
+    line0 = f"{Color.CYAN}{Effect.BOLD}{__package_name__.upper()}  {Effect.RESET}{Effect.BOLD}{Effect.ITALIC}{__version__} {Effect.RESET}"
+    line1 = f"{Color.CYAN}{Effect.BOLD}Author:{Effect.RESET} {__author__} {Effect.ITALIC}<{__email__}>{Effect.RESET}"
+
+    lenght = 48
+
+    print()
+
+    print(f"{Color.DARK_CYAN}{'='*(lenght)}{Effect.RESET}")
+    print(f" {line0}")
+    print(f" {line1}")
+    print(f"{Color.DARK_CYAN}{'='*(lenght)}{Effect.RESET}")
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"\n{Color.RED}==> {Color.WHITE}Exitting, KeyboardInterrupt ...{Color.RED}{Effect.RESET}\n")
+        exit(130)
+    except EOFError:
+        print(f"\n{Color.RED}==> {Color.WHITE}Exitting, EOFError ...{Color.RED}{Effect.RESET}\n")
+        exit(0)
